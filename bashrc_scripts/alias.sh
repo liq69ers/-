f02b13d0ddc5c273e3918eb7fbb73a22a88621e2
bob <liq69ers@hotmail.com>
2012-02-16 05:01:09 -0500

    new file:   alias.sh

---
+++ bashrc_scripts/alias.sh
@@ -0,0 +1,8 @@
+#	chmod
+alias 000='chmod 000'
+alias 640='chmod 640'
+alias 644='chmod 644'
+alias 755='chmod 755'
+alias 775='chmod 775'
+alias mx='chmod a+x'
+alias perm='stat --printf "%a %n \n "'	# requires a file name e.g. perm file
